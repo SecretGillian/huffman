65cd6aac49710c156d2eb4be8dbf4d2d4c30d6af
SecretGillian <secret.gillian.S3la@gmail.com>
2025-10-14 10:56:35 +0200

    ajout du debugu par l'uart

--- Src/compretion.c
+++ Src/compretion.c
@@ -3,8 +3,23 @@
 
 void occurence (uint8_t* chaine, uint32_t tab[NBR_CARACTERE])
 {
+	uint8_t y = 0;
+
+
+
 	for(uint8_t i = 0 ; i < NBR_CARACTERE ; i++)
 	{
+		y = 0;
+		tab[i] = 0;
 
+		while(chaine[y] != '\0')
+		{
+			if(chaine[y] == i)
+			{
+				tab[i]++;
+			}
+
+			y++;
+		}
 	}
 }
--- Src/main.c
+++ Src/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stm32f446xx.h>
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 #include "gpio.h"
 #include "usart.h"
@@ -16,20 +17,32 @@ int main(void)
 {
 	/*FPU_Init();
 	GPIO_Init();
-	USART2_Init();
 	SYSTICK_Init();*/
+	USART2_Init();
 
-	uint8_t texte[] = "aaaabbbccd";					//texte non compressé
-	uint8_t texteCompressé[TAILLE_MAX_COMPRESS];	//text compressé
-	uint32_t tabCaractere[NBR_CARACTERE] = {0};				//tableau du nombre d'occurrence de chaque caractère
-	uint32_t nbrCaractereTotal = 0;					//nombre de caractère totale dans le texte non compressé
-	uint32_t nbrCaractereDifferent = 0;				//nombre de caractère différent dans le texte nnon comprésé
-	struct noeud* arbreHffman[NBR_CARACTERE];					//arbre de huffman
+	uint8_t texte[] = "hello word\n\r";//"aaaabbbccd";					//texte non compressé
+	//uint8_t texteCompressé[TAILLE_MAX_COMPRESS];	//text compressé
+	//uint32_t tabCaractere[NBR_CARACTERE] = {0};				//tableau du nombre d'occurrence de chaque caractère
+	//uint32_t nbrCaractereDifferent = 0;				//nombre de caractère différent dans le texte nnon comprésé
+	//uint32_t nbrCaractereDifferent = 0;				//nombre de caractère différent dans le texte nnon comprésé
+	//uint32_t nbrCaractereTotal = 0;					//nombre de caractère totale dans le texte non compressé
+	//struct noeud* arbreHffman[NBR_CARACTERE];					//arbre de huffman
+
+	uint8_t i = 0;
 
 
-	while(1){
-		SYSTICK_Delay(100);
-		GPIOA->ODR ^= 1<<5;
-		printf("Hello\r\n");
-	}
+		while(texte[i] != '\0')
+		{
+			USART2->DR = texte[i];
+			while((USART2->SR & USART_SR_TXE) == 0);
+			i++;
+		}
+
+		printf("hello2\n");
+	/*while(1){
+		//SYSTICK_Delay(100);
+		//GPIOA->ODR ^= 1<<5;
+		//printf("Hello\r\n");
+
+	}*/
 }
